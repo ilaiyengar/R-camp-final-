34c0f6de7c0f1a9be67ed6a989f630506cf10ac7
Eleanor Lin <emlinking@gmail.com>
2016-08-19 13:52:54 -0400

    I deleted it

--- TCGA_DataProcessing.R
+++ TCGA_DataProcessing.R
@@ -44,10 +44,7 @@ getCorrelated = function(file1, file2) {
     ## same column
     fullTable = mergeTablesByPatientID(mRNA, miRNA)
     partialTable = as.matrix(splitmiRNATable(as.matrix(fullTable)))
-<<<<<<< HEAD
-=======
 
->>>>>>> c23a242e8355f9fb80cdbe6ab2d64a5543adfd4b
 
     fileOne = read.table(file1, header=T, row.names=1, stringsAsFactors=F)
     fileTwo = read.table(file2, header=T, row.names=1, stringsAsFactors=F)
